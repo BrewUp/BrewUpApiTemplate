d41326c79bb68a372cc7bd61ce304dd4233bfd1b
Alberto Acerbis <alberto.acerbis@gmail.com>
2025-03-27 04:40:29 +0100

    Add Swagger

---
+++ src/BrewUpApiTemplate/Modules/SwaggerModule.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace BrewUpApiTemplate.Modules;
+
+public sealed class SwaggerModule : IModule
+{
+	public bool IsEnabled => false;
+	public int Order => 0;
+	public IEnumerable<IModule> DependsOn => [];
+
+	public IServiceCollection Register(WebApplicationBuilder builder)
+	{
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen(setup => setup.SwaggerDoc("v1", new OpenApiInfo()
+		{
+			Description = "BrewUp",
+			Title = "BrewUp API",
+			Version = "v1",
+			Contact = new OpenApiContact
+			{
+				Name = "BrewUp"
+			}
+		}));
+
+		return builder.Services;
+	}
+
+	public WebApplication Configure(WebApplication endpoints) => endpoints;
+}
